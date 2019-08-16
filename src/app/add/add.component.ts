ca860265384d53efbab0021f60edc1f374ebb442
Venkateshsoma11 <somavenkatesh01@gmail.com>
2019-08-16 13:50:23 -0230

    add.ts and add.html

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -5,11 +5,16 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.css']
 })
-export class AddComponent implements OnInit {
+export class AddComponent  {
+  data=[]
+      
+  
+  addTask(form){
+  var task={"id": form.value.task,
+    "message":form.value.message
+   }
+   this.data.push(task);
 
-  constructor() { }
-
-  ngOnInit() {
   }
 
 }
